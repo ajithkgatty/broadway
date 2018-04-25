85fecfe45b11e4179ed2671fe38031a30b1ec4df
Ajith <ajithkgatty@gmail.com>
2018-04-25 19:49:51 +0530

    adding play controller actions and some view files

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,4 +1,19 @@
 class PlaysController < ApplicationController
   def index
+  	@plays = Play.all
+  end
+
+  def new
+  	@play = Play.new
+  end
+
+  def create
+  	@play = Play.new(play_params)
+  end
+
+  private
+
+  def play_params
+  	params.requrie(:play).permit(:title, :description, :director)
   end
 end
